fd4a2776ecbdebffddc47a144c9dccf384e9b2e6
Robert <rbezaire@mail.csuchico.edu>
2016-03-09 23:05:01 -0800

    Working Properly base

--- sprjkt/whiteboard/app/models/user.rb
+++ sprjkt/whiteboard/app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   
-  has_many :layers
   has_many :spaces
+  has_many :layers
 
 end
